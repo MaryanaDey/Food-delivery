65052d752f692c849a630ef38cbbbae730c6cc8b
Maryana Dey <Dey.maryana1989@gmail.com>
2024-03-23 18:31:38 +0100

    add icoms

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 // import "./Navbar.css";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={assets} alt="" />
+      <img src={assets.logo} alt="" />
     </div>
   );
 };
