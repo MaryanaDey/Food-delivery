3d65716f75d55aadb5dcecaa4fa06c76dec43e64
Maryana Dey <Dey.maryana1989@gmail.com>
2024-04-09 12:12:36 +0200

    add Cart and LiginPopUp

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            vitae explicabo minima laboriosam doloribus veniam at sunt, unde
+            ullam asperiores officiis repellendus, sint nulla quas modi vel
+            cumque nam dolor, ducimus placeat tempora? Officia dolor, maxime
+            debitis nisi cum soluta?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyrigtt 2024 @ Tomato.com - All Raght Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
