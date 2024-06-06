59ce20efcc0e4e39e094671d42415e9a6829a306
Maryana Dey <Dey.maryana1989@gmail.com>
2024-06-06 12:37:59 +0200

    create admin

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -4,8 +4,9 @@ import { assets } from "../../assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const Add = () => {
-  const url = "http://localhost:4000";
+const Add = ({ url }) => {
+  // const url = "http://localhost:4000";
+
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -31,8 +32,10 @@ const Add = () => {
     formData.append("description", data.description);
     formData.append("price", Number(data.price));
     formData.append("category", data.category);
-    formData.append("image", data.image);
+    formData.append("image", image);
+
     const response = await axios.post(`${url}/api/food/add`, formData);
+
     if (response.data.success) {
       setData({
         name: "",
